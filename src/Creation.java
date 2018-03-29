2e4f1dfbc29c7171d26cf7d0327cd6b5c750d0af
efremov-kv <efremov_kv@mail.ru>
2018-03-29 14:27:56 +0300

    output corrected

--- src/Creation.java
+++ src/Creation.java
@@ -4,15 +4,15 @@
 
 public class Creation {
     public static void main (String args[]) {
-        Weight creation1 = new Weight(10, 18, 20, 1.025);
-        Weight creation2 = new Weight(10, 1.025);
+        Weight creationParallelepiped = new Weight(10, 15, 4, 1.025);
+        Weight creationCube = new Weight(15, 1.025);
 
-        System.out.println("Объём creation1 равен: " + creation1.volume());
-        System.out.println("Вес creation1 равен: " + creation1.weight());
+        System.out.println("Объём параллелепипеда равен: " + creationParallelepiped.volume());
+        System.out.println("Вес параллелепипеда равен: " + creationParallelepiped.weight());
         System.out.println();
 
-        System.out.println("Объём creation2 равен: " + creation2.volume());
-        System.out.println("Вес creation2 равен: " + creation2.weight());
+        System.out.println("Объём куба равен: " + creationCube.volume());
+        System.out.println("Вес куба равен: " + creationCube.weight());
         System.out.println();
 
         System.out.println("THE END");
